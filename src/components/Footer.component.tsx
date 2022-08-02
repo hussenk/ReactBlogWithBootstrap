6fbd6171d72345165bae30eecfb2b0a6c3250d57
hussenk <hussen199710@yahoo.com>
2022-08-02 16:16:33 +0200

    [A] Footer

--- src/components/Footer.component.tsx
+++ src/components/Footer.component.tsx
@@ -1,10 +1,24 @@
+import { Links } from "./Navbar.component";
+
 const Footer = () => {
-    return (
-      <div>
-        <h1>Footer</h1>
-      </div>
-    );
-  };
-  
-  
-  export default Footer;
+  return (
+    <div className="container">
+      <footer className="pt-4 my-md-5 pt-md-5 border-top">
+        <div className="row">
+          <div className="col-6 col-md">
+            <p>Linkin Park</p>
+            <small className="d-block mb-3 text-muted">© 1996-2022</small>
+          </div>
+          <div className="col-6 col-md">
+            <h5>Visit</h5>
+            <ul className="list-unstyled text-small">
+              {<Links css={"text-muted"} />}
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
