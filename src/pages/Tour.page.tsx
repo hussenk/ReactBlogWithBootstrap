2a2e62f125fbee47f99c4c8db07024c46a5b087c
hussenk <hussen199710@yahoo.com>
2022-08-02 19:09:59 +0200

    [U] tour page

--- src/pages/Tour.page.tsx
+++ src/pages/Tour.page.tsx
@@ -5,27 +5,85 @@ const Tour = () => {
     <div>
       <div className="container mb-2">
         <TitlePage title={"Our Tours"} />
-        <div className="row mt-3">
-          <div className="col-6 col">
-            <div className="card" style={{ width: "18rem" }}>
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/4/42/LinkinParkBerlin2010.jpg"
-                className="card-img-top"
-                alt="..."
-              />
-              <div className="card-body">
-                <p className="card-text">2010</p>
+        <div className="grid text-center mt-3">
+          <div className="">
+            <div className="g-col-12 ">
+              <div
+                id="carouselExampleIndicators"
+                className="carousel slide"
+                data-ride="carousel"
+              >
+                <ol className="carousel-indicators">
+                  <li
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="0"
+                    className="active"
+                  ></li>
+                  <li
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="1"
+                  ></li>
+                  <li
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="2"
+                  ></li>
+                </ol>
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img
+                      className="d-block w-100"
+                      src="https://www.billboard.com/wp-content/uploads/media/chester-bennington-linkin-park-Projekt-Revolution-Tour-2008-a-billboard-1548.jpg"
+                      alt="First slide"
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      className="d-block w-100"
+                      src="http://www.alternativenation.net/wp-content/uploads/2018/12/linkinparkstreet.jpg"
+                      alt="Second slide"
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      className="d-block w-100"
+                      src="https://upload.wikimedia.org/wikipedia/commons/4/42/LinkinParkBerlin2010.jpg"
+                      alt="Third slide"
+                    />
+                  </div>
+                </div>
+                <a
+                  className="carousel-control-prev"
+                  href="#carouselExampleIndicators"
+                  role="button"
+                  data-slide="prev"
+                >
+                  <span
+                    className="carousel-control-prev-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span className="sr-only">Previous</span>
+                </a>
+                <a
+                  className="carousel-control-next"
+                  href="#carouselExampleIndicators"
+                  role="button"
+                  data-slide="next"
+                >
+                  <span
+                    className="carousel-control-next-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span className="sr-only">Next</span>
+                </a>
               </div>
             </div>
-
-            <div className="col-6 col-md">
-              <p className="lead">
-                Quickly build an effective pricing table for your potential
-                customers with this Bootstrap example. It's built with default
-                Bootstrap components and utilities with little customization.
-              </p>
-            </div>
-            
+          </div>
+          <div className="g-col-12 ">
+            <p className="lead">
+              Quickly build an effective pricing table for your potential
+              customers with this Bootstrap example. It's built with default
+              Bootstrap components and utilities with little customization.
+            </p>
           </div>
         </div>
       </div>
