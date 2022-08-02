3f3d9e7d5d7ded241bb9d2e2335419d2a6d3db6d
hussenk <hussen199710@yahoo.com>
2022-08-02 15:29:39 +0200

    [U] routes in navbar

--- src/components/Navbar.component.tsx
+++ src/components/Navbar.component.tsx
@@ -7,25 +7,25 @@ const Navbar = () => {
 
 export default Navbar;
 
-const nav1 = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <nav className="nav justify-content-center navbar-light">
-          <Link className="nav-link active" to={"#"}>
-            Active link
-          </Link>
-          <Link className="nav-link" to={"#"}>
-            Link
-          </Link>
-          <Link className="nav-link disabled" to={"#"}>
-            Disabled link
-          </Link>
-        </nav>
-      </BrowserRouter>
-    </div>
-  );
-};
+// const nav1 = () => {
+//   return (
+//     <div>
+//       <BrowserRouter>
+//         <nav className="nav justify-content-center navbar-light">
+//           <Link className="nav-link active" to={"#"}>
+//             Active link
+//           </Link>
+//           <Link className="nav-link" to={"#"}>
+//             Link
+//           </Link>
+//           <Link className="nav-link disabled" to={"#"}>
+//             Disabled link
+//           </Link>
+//         </nav>
+//       </BrowserRouter>
+//     </div>
+//   );
+// };
 const nav2 = () => {
   return (
     <div>
@@ -48,13 +48,10 @@ const nav2 = () => {
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav">
-                <li>h</li>
-                <li>2</li>
-                <li>3</li>
-                {/* <ListItem css="" to="home" text="Home" />
+                <ListItem css="" to="home" text="Home" />
                 <ListItem css="" to="aboutus" text="About US" />
                 <ListItem css="" to="tour" text=" Our Tours" />
-                <ListItem css="" to="buy" text="Our Stuff" /> */}
+                <ListItem css="" to="buy" text="Our Stuff" />
               </ul>
             </div>
           </div>
