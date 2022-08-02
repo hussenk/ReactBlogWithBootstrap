565af62fa1710cdb50baf65c66498bf329e81f16
hussenk <hussen199710@yahoo.com>
2022-08-02 17:15:36 +0200

    [U] home page

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -6,6 +6,26 @@ const Home = () => {
       <div className="container mb-2">
         <TitlePage title={"Home"} />
       </div>
+      <div className="text-center">
+        <h1 className="display-4">Welcome</h1>
+        <div className="container">
+          <p className="lead item-center ">
+            Welcome page Welcome page Welcome page Welcome page Welcome page
+            Welcome page Welcome page Welcome page Welcome page Welcome page
+            Welcome page Welcome page Welcome page Welcome page Welcome page
+            Welcome page Welcome page Welcome page Welcome page Welcome page
+            Welcome page Welcome page Welcome page Welcome page Welcome page
+            Welcome page Welcome page Welcome page Welcome page Welcome page
+            Welcome page Welcome page Welcome page Welcome page Welcome page
+            Welcome page Welcome page Welcome page Welcome page Welcome page
+            Welcome page Welcome page Welcome page Welcome page Welcome page
+            Welcome page Welcome page Welcome page Welcome page Welcome page
+            Welcome page Welcome page Welcome page Welcome page Welcome page
+            Welcome page Welcome page Welcome page Welcome page Welcome page
+            Welcome page
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
